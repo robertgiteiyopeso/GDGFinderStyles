5ea849518dcf83d027d9fa4ce1850dec7e6bd402
Robert Gitei <robert.gitei@yopeso.com>
2022-07-18 16:32:31 +0300

    Adds click listener to the Floating Action Button

--- app/src/main/java/com/example/android/gdgfinder/home/HomeFragment.kt
+++ app/src/main/java/com/example/android/gdgfinder/home/HomeFragment.kt
@@ -21,7 +21,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
+import com.example.android.gdgfinder.R
 import com.example.android.gdgfinder.databinding.HomeFragmentBinding
 
 class HomeFragment : Fragment() {
@@ -38,6 +41,17 @@ class HomeFragment : Fragment() {
     ): View? {
         val binding = HomeFragmentBinding.inflate(inflater)
         viewModel = ViewModelProvider(this).get(HomeViewModel::class.java)
+
+        viewModel.navigateToSearch.observe(viewLifecycleOwner,
+        Observer<Boolean> {
+            navigate -> if(navigate) {
+                val navController = findNavController()
+                navController.navigate(R.id.action_homeFragment_to_gdgListFragment)
+                viewModel.onNavigatedToSearch()
+        }
+        })
+
+        binding.viewModel = viewModel
 
         return binding.root
     }
